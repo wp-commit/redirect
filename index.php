d4df93d845aa8aaff765537b7690daf1673cfade
wp-commit <mbeas976@gmail.com>
2026-07-29 19:56:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,2 @@
 <?php
-// Cek apakah sedang di root (bukan di halaman lain)
-if ($_SERVER['REQUEST_URI'] === '/' || $_SERVER['REQUEST_URI'] === '/index.php') {
-    header("Location: https://www.youtube.com/watch?v=eTM0t7Nnz_0&list=RDeTM0t7Nnz_0&start_radio=1", true, 301);
-    exit();
-}
-?>
+https://www.youtube.com/watch?v=eTM0t7Nnz_0&list=RDeTM0t7Nnz_0&start_radio=1
